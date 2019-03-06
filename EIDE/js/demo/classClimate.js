d42b8b4449668719f7e5b367e924488f1345e9bb
Carla De Lira <carlamdelira@gmail.com>
2019-03-05 16:55:45 -0800

    Added more features in classroom climate
    ~ More actions
    ~ Contacting students

---
+++ EIDE/js/demo/classClimate.js
@@ -0,0 +1,26 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'pie',
+  data: {
+    labels: ["Happy", "Confused", "Engaged"],
+    datasets: [{
+      data: [50,25,25],
+      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: { 
+  
+  legend: {
+      display: false
+    },
+
+
+  },
+});
